befa868730526ad389a181c3579ad5b11e40f51a
yisenlin <ysl.130@qq.com>
2019-07-28 19:24:44 +0800

    tab-width

--- init.el
+++ init.el
@@ -10,6 +10,11 @@
 ;; 行号显示
 (global-linum-mode 1)
 
+(setq-default tab-width 4)
+
+;;可视化空白符
+(global-whitespace-mode 1)
+
 ;; 关闭备份文件
 (setq make-backup-files nil)
 
@@ -29,8 +34,25 @@
 (require 'evil)
 (evil-mode 1)
 (load-theme 'monokai 1)
+(require 'racket-mode)
+(setq racket-racket-program "racket")
+(setq racket-raco-program "raco")
+
+
 
 ;;set gc threshold in startup and after
 (let ((n (* 32 1024 1024))
       (i (* 128 1024 1024)))
   (setq gc-cons-threshold i))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (go-mode racket-mode vlf monokai-theme evil))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
