db3b8d9011990638b57cc661d81412a749d6d2bc
yisenlin <ysl.130@qq.com>
2020-05-14 23:46:11 +0800

    use use-package to rewrite

--- init.el
+++ init.el
@@ -5,11 +5,12 @@
 (require 'init-benchmarking)
 
 (setq inhibit-startup-message t)
+
+(show-paren-mode t)
 (menu-bar-mode 0)
-;(tool-bar-mode nil)
+(tool-bar-mode 0)
 
 (global-hl-line-mode 1)
-(global-linum-mode 1)
 
 (setq-default tab-width 4)
 
@@ -17,42 +18,27 @@
 
 (setq make-backup-files nil)
 
-;; key bind
 (defun open-init-file()
   (interactive)
   (find-file "~/.emacs.d/init.el"))
 
-(global-set-key (kbd "<f2>") 'open-init-file)
-
-;; china mirror
-(require 'package)
-(setq package-archives '(("gnu" . "http://elpa.emacs-china.org/gnu/")
-			 ("melpa" . "http://elpa.emacs-china.org/melpa/")))
+(setq package-archives
+	  '(("gnu" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+		("melpa" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
 
 (package-initialize)
 
-(require 'cl)
-
-(defvar my/packages '(
-					  evil
-					  monokai-theme
-					  ))
-(setq package-selected-packages my/packages)
-(defun my/packages-install-p ()
-  (loop for pkg in my/packages
-		when (not (package-installed-p pkg)) do (return nil)
-		finally (return t)))
-
-(unless (my/packages-install-p)
-  (message "%s" "Refreshing package database...")
+(unless (package-installed-p 'use-package)
   (package-refresh-contents)
-  (dolist (pkg my/packages)
-	(when (not (package-installed-p pkg))
-	  (package-install pkg))))
+  (package-install 'use-package))
 
-;;包配置
-(evil-mode 1)
-(load-theme 'monokai 1)
+(use-package evil
+			 :ensure t
+			 :config (evil-mode 1))
+
+(use-package monokai-theme
+  :ensure t
+  :config (load-theme 'monokai 1))
 
 ;;set gc threshold in startup and after
 (let ((n (* 32 1024 1024))
